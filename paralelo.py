2a051f4c523ffb96919ab8b393a1224d664b627f
chompatas <47321861+chompatas@users.noreply.github.com>
2019-02-05 09:56:49 -0200

    Update paralelo.py

--- paralelo.py
+++ paralelo.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 #
 # This is the solution code for the *echo server*.
+import webapp2
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(self.path[1:].encode())
 
-
-class EchoHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # First, send a 200 OK response.
-        self.send_response(200)
-
-        # Then send headers.
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
-        self.end_headers()
-
-        # Now, write the response body.
-        self.wfile.write(self.path[1:].encode())
-
-if __name__ == '__main__':
-    server_address = ('', 5000)  # Serve on all addresses, port 8000.
-    httpd = HTTPServer(server_address, EchoHandler)
-    httpd.serve_forever()
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
