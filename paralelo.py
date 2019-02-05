2fe21869d8d5208262a30797a9f311b0348c25cc
chompatas <47321861+chompatas@users.noreply.github.com>
2019-02-05 17:41:17 -0200

    Update paralelo.py

--- paralelo.py
+++ paralelo.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
-#
-# This is the solution code for the *echo server*.
+
 import webapp2
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Ola mundo')
+        self.response.write('Hello, World!')
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
