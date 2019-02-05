d4aa1840dba2229df9070f0b522e6f5ac1667314
chompatas <47321861+chompatas@users.noreply.github.com>
2019-02-05 18:50:21 -0200

    Update paralelo.py

--- paralelo.py
+++ paralelo.py
@@ -4,14 +4,14 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write("""<!DOCTYPE html>
+        self.response.write('<!DOCTYPE html>
   <title>Message Board</title>
   <form method="POST" action="http://localhost:8000/">
     <textarea name="message"></textarea>
     <br>
     <button type="submit">Post it!</button>
   </form>')
-"""
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
